1a390c41b77190f4c91c5faaa55bc80aa00721e8
jeff3071 <jeff563715@gmail.com>
2024-10-27 14:35:02 +0800

    add more test case

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import pytest
 
 from baseball_stats_python.utils.utils import validate_date_format, validate_date_range
@@ -7,6 +9,10 @@ def test_validate_date_format():
     with pytest.raises(ValueError) as e:
         validate_date_format('2024/07/01')
     assert "does not match format '%Y-%m-%d'" in str(e.value)
+
+    date = validate_date_format('2024-07-01')
+    assert isinstance(date, datetime)
+    assert date == datetime(2024, 7, 1)
 
 
 def test_validate_date_range():
